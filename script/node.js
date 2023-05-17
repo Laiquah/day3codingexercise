1268050688abbb0821183e82bf258fc2c1a931ee
Laiquah <cotielaiquah@gmail.com>
2023-05-17 08:59:51 +0200

    added changes to node.js

---
+++ script/node.js
@@ -0,0 +1,8 @@
+var number = '5';
+number = '50';
+console.log(number);
+// console.log(typeof number);
+
+var datatypes = ['Strings', 'Booleans', 'Numbers', 'Undefined', 'Null'];
+// console.log(datatypes);
+console.log(typeof datatypes) 
